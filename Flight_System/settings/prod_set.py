58a4eb5faa9a5892cd0b62fffb0951217c21aa9c
Kevans86 <kevevans1986@gmail.com>
2023-01-29 20:21:04 +0200

    fixes

---
+++ Flight_System/settings/prod_set.py
@@ -0,0 +1,27 @@
+from .common import *
+import os
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DROPBOX_APP_KEY = os.environ.get('DROPBOX_APP_KEY')
+DROPBOX_APP_SECRET= os.environ.get('DROPBOX_APP_SECRET')
+DROPBOX_OAUTH2_REFRESH_TOKEN=os.environ.get('DROPBOX_OAUTH2_REFRESH_TOKEN')
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app' ]
+
+#Database
+#https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
